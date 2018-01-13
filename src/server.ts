6cfd24a4eb89fec63d93b356320aa27fe835c152
blaster <g.glazewski@gmail.com>
2018-01-13 12:19:13 +0100

    Adds nodemon config.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 
 import * as express from 'express';
+import * as http from 'http';
 
 const app = express();
+const server = new http.Server(app);
+
+server.listen(process.env.PORT || 8081, function() {
+  console.log(`Listening on ${server.address().port}`);
+});
 
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
-
-app.listen(8080, () => console.log('Listening ON port 8080!'));
